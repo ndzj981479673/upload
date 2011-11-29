b419c84a16c709c15aeeca49fcaa720ddfcbc6a4
zj <ndzj981479673@126.com>
2011-11-29 19:10:28 +0800

    update recommend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111129013803) do
+ActiveRecord::Schema.define(:version => 20111129103232) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20111129013803) do
     t.text     "description"
     t.string   "classification"
     t.string   "title"
+    t.boolean  "recommend"
   end
 
 end
